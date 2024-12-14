4a124bd4274f8dc98b4bb26ef0f9c4624bfc06f0
Tzekov <stanislav.tzekov@dxc.com>
2024-12-14 12:02:12 +0200

    Add login form

--- users/views.py
+++ users/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import LoginForm
 
 # Create your views here.
-
 def index(request):
     return render(request, "users/index.html")
+
+
+def login(request):
+    form = LoginForm()
+    return render(request, "users/login.html", { "form": form })
