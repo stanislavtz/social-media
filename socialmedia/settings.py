0569980239b511de3d9ce43d4fb7611ab716b7d7
Tzekov <stanislav.tzekov@dxc.com>
2024-12-16 22:18:12 +0200

    Add password reset functionality

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -131,3 +131,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
+
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,8 @@ urlpatterns = [
     path("profile/", views.user_profile, name="profile"),
     path("password_change/", PasswordChangeView.as_view(template_name="users/password_change.html"), name="password_change"),
     path("password_change/done", PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name="password_change_done"),
+    path("password_reset/", PasswordResetView.as_view(template_name="users/password_reset.html"), name="password_reset"),
+    path("password_reset/done", PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"), name="password_reset_done"),
+    path("password_reset/confirm/<uidb64>/<token>", PasswordResetConfirmView.as_view(template_name="users/password_reset_confirm.html"), name="password_reset_confirm"),
+    path("password_reset/complete", PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"), name="password_reset_complete"),
 ]
