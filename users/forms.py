6d62eb8775966f0c9262d7a0c9812e7685fbd8f2
Tzekov <stanislav.tzekov@dxc.com>
2024-12-18 16:43:08 +0200

    Add Profile form

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class RegisterForm(forms.ModelForm):
@@ -14,4 +15,9 @@ class RegisterForm(forms.ModelForm):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=255)
     password = forms.CharField(widget=forms.PasswordInput)
-    
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["img"]
