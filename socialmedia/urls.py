c2c4b1ca475b5356d89bf12d4b75445575feeeae
Tzekov <stanislav.tzekov@dxc.com>
2024-12-20 19:43:13 +0200

    Add path to posts urls

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -23,7 +23,8 @@ from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', users_views.index, name="index"),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('posts/', include('posts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
