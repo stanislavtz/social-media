435d4e03e4e04e7843ccc8be8e4fa7b0113a12ed
Tzekov <stanislav.tzekov@dxc.com>
2024-12-20 10:27:09 +0200

    Create Post model

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Post
+        list_display_links = ("id", "title", "slug", "created")
+        list_filter = ("created",)
+
+
+admin.site.register(Post, PostAdmin)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    image = models.ImageField(upload_to="posts", null=True, blank=True)
+    title = models.CharField(max_length=255)
+    caption = models.TextField(blank=True)
+    slug = models.SlugField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
