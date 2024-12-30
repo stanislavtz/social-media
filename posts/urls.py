defbe91d1226a28d6c522e08413239fa78407074
Tzekov <stanislav.tzekov@dxc.com>
2024-12-30 09:36:09 +0200

    Add like path

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("create", views.create_post, name="create_post"),
+    path("create/", views.create_post, name="create_post"),
     path("user_posts/", views.users_post, name="user_posts"),
-    path("<slug:slug>", views.post_details, name="post_details")
+    path("like/", views.like_post, name="like_post"),
+    path("<slug:slug>", views.post_details, name="post_details"),
 ]
