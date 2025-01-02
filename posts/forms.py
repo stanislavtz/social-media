dc9eda9384730c057454c81624a185a20a04de34
Tzekov <stanislav.tzekov@dxc.com>
2025-01-02 11:02:28 +0200

    Crate comment form

--- posts/forms.py
+++ posts/forms.py
@@ -1,8 +1,15 @@
 from django import forms
-from .models import Post
+from .models import Post, Comment
 
 
 class CreatePostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "image", "caption"]
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+        
